dec46fc15f444598991646d43c65906b98890b67
Fredrik Wallenius <fredrik.wallenius@accedo.tv>
2014-04-15 22:17:44 +0200

    Adding description

--- modulesExample2.js
+++ modulesExample2.js
@@ -1,3 +1,7 @@
+// This example shows how to setup a more advanced application module
+// in its own folder with a package.json file defining an entry point
+// Run with:
+// export DEBUG=simple-cache-library:* && node modulesExample2.js
 
 
 var Cache = require('./app_modules/cache')
@@ -13,6 +17,12 @@ catCache.put('MrSmith', {color: "Black"});
 catCache.put('Nelson', {color: "White"});
 dogCache.put('Rex', {dangerous: true});
 
-console.log('I have ' + catCache.size() + ' cats and ' + dogCache.size() + ' dogs.');
+console.log('I have ' + catCache.size() + ' cats and ' + dogCache.size() + ' dog.');
+// I have 2 cats and 1 dog.
+
 console.log('Nelson is ' + catCache.get('Nelson').color);
+// Nelson is White
+
 console.log('Do I have a dog named MrSmith? ' + dogCache.get('MrSmith'));
+// Do I have a dog named MrSmith? undefined
+
